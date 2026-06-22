84b2e67af1d9144342f5df27ab5aba6b53500473
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-22 13:57:33 +0000

    feat(realm): Add ingnore_changes example

--- realm.tf
+++ realm.tf
@@ -1,8 +1,16 @@
 resource "keycloak_realm" "example" {
   lifecycle {
     prevent_destroy = true
+    ignore_changes = [
+      attributes["created_at"]
+    ]
   }
 
   realm   = "example"
   enabled = true
+
+  attributes = {
+    created_at = timestamp()
+    team       = "core"
+  }
 }
