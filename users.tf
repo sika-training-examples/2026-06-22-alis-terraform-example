f70745be873e61056ab81f771437a5e6410535b4
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-22 12:21:26 +0000

    chore: Rewrite to for each

--- users.tf
+++ users.tf
@@ -1,74 +1,31 @@
-resource "random_password" "admin" {
-  length  = 16
-  special = false
+locals {
+  users = {
+    "admin"  = ["Admin", "Admin"]
+    "ondrej" = ["Ondrej", "Sika"]
+    "dela"   = ["Dela", "Dela"]
+  }
 }
 
-resource "keycloak_user" "admin" {
+resource "slu_random_password" "password" {
+  for_each = local.users
+}
+
+resource "keycloak_user" "user" {
+  for_each = local.users
+
   realm_id = keycloak_realm.example.realm
 
-  username = "admin"
+  username = each.key
   enabled  = true
 
-  email          = "admin@sikademo.com"
+  email          = "${each.key}@sikademo.com"
   email_verified = true
-  first_name     = "Admin"
-  last_name      = "Admin"
+  first_name     = each.value[0]
+  last_name      = each.value[1]
 
   initial_password {
-    value     = random_password.admin.result
+    value     = slu_random_password.password[each.key].result
     temporary = false
   }
 }
 
-resource "slu_random_password" "ondrej" {}
-
-resource "keycloak_user" "ondrej" {
-  realm_id = keycloak_realm.example.realm
-
-  username = "ondrej"
-  enabled  = true
-
-  email          = "ondrej@sikademo.com"
-  email_verified = true
-  first_name     = "Ondrej"
-  last_name      = "Sika"
-
-  initial_password {
-    value     = slu_random_password.ondrej.result
-    temporary = false
-  }
-}
-
-resource "slu_random_password" "dela" {}
-
-resource "keycloak_user" "dela" {
-  realm_id = keycloak_realm.example.realm
-
-  username = "dela"
-  enabled  = true
-
-  email          = "dela@sikademo.com"
-  email_verified = true
-  first_name     = "Dela"
-  last_name      = "Dela"
-
-  initial_password {
-    value     = slu_random_password.dela.result
-    temporary = false
-  }
-}
-
-output "initial_password_admin" {
-  value     = random_password.admin.result
-  sensitive = true
-}
-
-output "initial_password_ondrej" {
-  value     = slu_random_password.ondrej.result
-  sensitive = true
-}
-
-output "initial_password_dela" {
-  value     = slu_random_password.dela.result
-  sensitive = true
-}
