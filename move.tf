5c90cad75f4288b8af0adae1125c4f238de61e3d
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-22 12:21:51 +0000

    feat: Add state moves

---
+++ move.tf
@@ -0,0 +1,9 @@
+moved {
+  from = slu_random_password.dela
+  to   = slu_random_password.password["dela"]
+}
+
+moved {
+  from = slu_random_password.ondrej
+  to   = slu_random_password.password["ondrej"]
+}
